50465bc155e00dce16fd60c4cd46faf188713b1f
Angel21-ctrl-Z <angel21tech@gmail.com>
2024-11-03 16:37:46 -0800

    modules

---
+++ boot_game/main.py
@@ -0,0 +1,16 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+
+def main():
+
+    print("Starting asteroids!")
+
+print(SCREEN_HEIGHT, SCREEN_WIDTH)
+
+    
+if __name__ == "__main__":
+    main()
