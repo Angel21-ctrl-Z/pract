517b836d2edc8890779910b31f380d3e09d9a2e2
Angel21-ctrl-Z <angel21tech@gmail.com>
2024-11-04 20:09:14 -0800

    screen_pygame

--- boot_game/main.py
+++ boot_game/main.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
 
 # this allows us to use code from
 # the open-source pygame library
@@ -7,11 +7,21 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
 
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen,(0,0,0))
+        pygame.display.flip()
+
     print("Starting asteroids!")
 
-    print(f"Screen width: {SCREEN_WIDTH} 
-          Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
-    
 if __name__ == "__main__":
     main()
