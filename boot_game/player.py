f487e4f72d509a02b2639cd13538bbf0a1ba4617
Angel21-ctrl-Z <angel21tech@gmail.com>
2024-11-13 15:39:56 -0800

    cooldown

--- boot_game/player.py
+++ boot_game/player.py
@@ -10,7 +10,9 @@ class Player(CircleShape):
 
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
+
         self.rotation = 0
+        self.cooldown = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -25,6 +27,9 @@ class Player(CircleShape):
         pygame.draw.polygon(screen,"white", self.triangle(), 2)
     
     def update(self, dt):
+            
+            self.cooldown -= dt
+
             keys = pygame.key.get_pressed()
 
             if keys[pygame.K_a]:
@@ -49,6 +54,10 @@ class Player(CircleShape):
     
     def shoot(self):
 
-        shot = Gun(self.position.x,self.position.y)
+        if self.cooldown <= 0:
+            
+            self.cooldown += PLAYER_SHOOT_COOLDOWN
 
-        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            shot = Gun(self.position.x,self.position.y)
+
+            shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
