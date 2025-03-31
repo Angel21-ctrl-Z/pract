b06acbde1e91d7e125c1a5ef741f694b2ff0266e
Angel21-ctrl-Z <angel21tech@gmail.com>
2025-03-31 14:20:44 -0800

    update_dic_def_output

--- word_learning/Main.py
+++ word_learning/Main.py
@@ -1,13 +1,15 @@
 from Meaning.Definition import definition
 
+word = definition()
 
 def main():
-    print("Welcome to the Word Learning Program!")
-    print("This program will help you learn new words and their meanings.")
-    definition.Create(definition)
+    print("\nWelcome to the Word Learning Program!")
+    print("This program will help you learn new words and their meanings.\n")
+    word.Create()
     
-    print("Word added successfully!")
-    print(f"Word: {definition.word} - Meaning: {definition.meaning} - Example: {definition.example}")
+    print("Word added successfully!\n")
+    word.Output()
+    
     print("Thank you for using the Word Learning Program!")
 
 if __name__ == "__main__":
--- word_learning/Meaning/Definition.py
+++ word_learning/Meaning/Definition.py
@@ -7,12 +7,22 @@ class definition:
     def Create(self):
         self.word = input("Enter a new word: ")
         print(f"Word: {self.word}")
+        
         self.meaning = input("Enter the meaning of the word: ")
         self.example = input("Enter an example sentence using the word: ")
         print(f"Meaning: {self.meaning}")
         print(f"Example: {self.example}")
 
+    def Output(self):
+        print(f"Word: {self.word}")
+        print(f"Meaning: {self.meaning}")
+        print(f"Example: {self.example}\n")
+    
+    def is_it(self):
+        pass
+        
+
 if __name__ == "__main__":
     new_word = definition()
     new_word.Create()
-    print(f"New word created: {new_word.word}, Meaning: {new_word.meaning}, Example: {new_word.example}")
+    new_word.Output()
