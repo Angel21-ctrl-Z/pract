02b85fa32ad4e5f38aac5b38f1508ed536b98205
Angel21-ctrl-Z <angel21tech@gmail.com>
2025-04-03 00:02:25 -0800

    Add functional category classes: Pronoun, Determiner, Conjunstion, Preposition; enhance FunctionalCat with dynamic category handling

---
+++ word_learning/Meaning/Functional_cat/Preposition.py
@@ -0,0 +1,10 @@
+from functional_cat import FunctionalCat
+import os
+
+class Preposition(FunctionalCat):
+    def __init__(self):
+        super().__init__()
+        self.pronoun_type = None
+
+    def mark(self):
+        print(self.function)
