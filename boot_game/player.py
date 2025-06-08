f6eef413a34cbb376b7b66069c319600db682f53
Angel21-ctrl-Z <angel21tech@gmail.com>
2025-06-08 15:24:25 -0700

    Adjust asteroid spawn timer to introduce a delay and refine player triangle rendering for improved visuals

--- boot_game/player.py
+++ boot_game/player.py
@@ -19,8 +19,8 @@ class Player(CircleShape):
         pygame.draw.polygon(screen,"white", self.triangle(), 2)
 
     def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        forward = pygame.Vector2(0, .5).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.4
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
